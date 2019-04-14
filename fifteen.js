1512dc598a7c5bfaf81edfe84d99c9abde84a202
Byron Mouen <42878404+Birohn@users.noreply.github.com>
2019-04-14 15:40:59 -0400

    end game fix

--- fifteen.js
+++ fifteen.js
@@ -13,6 +13,9 @@
 
 // Global variables
 var gameBlock;
+
+var temp;
+
 var imageArray = ['tomandjerry.png', 'mario.png', 'scooby.png', 'knd.png'];
 var path = "./img/";
 var imgStr;
@@ -33,7 +36,8 @@ window.onload = function () {
 	
 	// get a random image
 	imgRandom(imageArray);
-
+	//hide buttonWin
+	$("#buttonWin").hide();
 	// create the individual 15 div blocks
 	for (var i = 0; i < 15; i++) {
 		var block = document.createElement("div");
@@ -75,6 +79,8 @@ window.onload = function () {
 			movePosition(parseInt(this.style.top), parseInt(this.style.left),this.innerHTML -1);
 		});	
 	}
+	//start timer on load
+	setInterval(timer, 1000);
 	//background audio play and loop
 	backgroundSound.addEventListener('ended', function() {
 		this.currentTime = 0;
@@ -82,6 +88,7 @@ window.onload = function () {
 	},false);
 	backgroundSound.play();
 	// shuffle functionality
+
 	var shuffle = document.getElementById("shuffle"); 
 	shuffle.onclick = function() 
 	{
@@ -149,11 +156,11 @@ window.onload = function () {
 				}
 			}
 		}
-		
-	};
+
+	//show the win button
+	 showButton();
 	
-	//start timer on load
-	setInterval(timer, 1000);
+	};
 	// image selection 
 	$( "#showValue" ).click(function() {
 		imgStr = select.value;
@@ -164,8 +171,7 @@ window.onload = function () {
 			});
 		}
 	});
-	
-};
+}
 
 // random image generator
 function imgRandom() {
@@ -260,42 +266,51 @@ var numMoves =0;
 //uses helper functions above and move piece if true. Takes top , left, and array position of gamePiece.
 
 function movePosition(x , y ,position) {
+	if(isInTransit == true) {
+		return;
+	}
 	if(checkRight(x, y)) {
 		//gameBlock[position].style.left = parseInt(gameBlock[position].style.left) + 100 +"px";
 		animationRight(position);
 		document.getElementById("numMovesLabel").innerHTML=++numMoves;
+		
 		return;
 	}
 	if(checkLeft(x, y)){
 		//gameBlock[position].style.left = parseInt(gameBlock[position].style.left) - 100 +"px";
 		animationLeft(position);
 		document.getElementById("numMovesLabel").innerHTML=++numMoves;
+		
 		return;
 	}
 	if(checkTop(x, y)) {
 		//gameBlock[position].style.top = parseInt(gameBlock[position].style.top) -100 +"px";
 		animationUp(position);
 		document.getElementById("numMovesLabel").innerHTML=++numMoves;
+		
 		return;
 	}
 	if(checkBottom(x, y)) {
 		//gameBlock[position].style.top = parseInt(gameBlock[position].style.top) +100 +"px";
 		animationDown(position);
 		document.getElementById("numMovesLabel").innerHTML=++numMoves;
+		
 		return;
 	}
+	
 }
+var isInTransit= false;
 //animation functions for movement of gamePieces into their new positions
 function animationUp(position) {
 	var currentPosition = 0;
 	var frameByFrame= setInterval(frame, 10);
 	function frame() {
 		if(currentPosition ==25) {
-			
+			isInTransit= false;
 			clearInterval(frameByFrame);
 		}
 		else {
-			
+			isInTransit= true;
 			currentPosition++;
 			gameBlock[position].style.top = parseInt(gameBlock[position].style.top) - 4 +"px";
 		}
@@ -307,11 +322,11 @@ function animationDown(position) {
 	var frameByFrame= setInterval(frame, 10);
 	function frame() {
 		if(currentPosition ==25) {
-			
+			isInTransit= false;
 			clearInterval(frameByFrame);
 		}
 		else {
-			
+			isInTransit= true;
 			currentPosition++;
 			gameBlock[position].style.top = parseInt(gameBlock[position].style.top) + 4 +"px";
 		}
@@ -323,11 +338,11 @@ function animationLeft(position) {
 	var frameByFrame= setInterval(frame, 10);
 	function frame() {
 		if(currentPosition ==25) {
-			
+			isInTransit= false;
 			clearInterval(frameByFrame);
 		}
 		else {
-			
+			isInTransit= true;
 			currentPosition++;
 			gameBlock[position].style.left = parseInt(gameBlock[position].style.left) - 4 +"px";
 		}
@@ -339,11 +354,11 @@ function animationRight(position) {
 	var frameByFrame= setInterval(frame, 10);
 	function frame() {
 		if(currentPosition ==25) {
-			
+			isInTransit= false;
 			clearInterval(frameByFrame);
 		}
 		else {
-			
+			isInTransit= true;
 			currentPosition++;
 			gameBlock[position].style.left = parseInt(gameBlock[position].style.left) + 4 +"px";
 		}
@@ -395,6 +410,7 @@ function formatTime(time) {
 }
 
 // shuffle helper functions
+
 function moveleft(x, y) 
 
 {
@@ -487,3 +503,62 @@ function movedown (x, y)
 	} 
 }
 
+
+function endGame()
+
+{
+	var msg = "You win!";
+	var ifTrue = true;
+
+	for (var i = 0; i < gameBlock.length; i++) //for each puzzle piece 
+	{
+
+		var top = parseInt(gameBlock[i].style.top);
+
+		var left = parseInt(gameBlock[i].style.left);
+
+
+		if (left != (i%4*100) || top != parseInt((i/4))*100) //checks if each piece matches its left and top position
+
+		{
+			console.log(gameBlock[i]);
+			ifTrue = false;
+
+			break;
+
+			//document.getElementById("formsg").innerHTML = msg;
+
+		}
+
+	}
+
+	return ifTrue;
+
+}
+//check to see if there is a win
+function checkWinGame() {
+	var msg= "You Win!"
+	if(endGame()) {
+		clearInterval(timer());
+		document.getElementById("formsg").innerHTML = msg +" Time: " + document.getElementById("minutesLabel").innerHTML + ":" + document.getElementById("secondsLabel").innerHTML + " Moves: " + numMoves;
+		$("#buttonWin").hide();
+		backgroundSound.pause();
+		var winSound = new Audio("audio/grunt-birthday-party.mp3");
+		winSound.play();
+		var img="img/win.png";
+		for (var i = 0; i < gameBlock.length; i++) {
+			$( gameBlock[i] ).css({
+				"backgroundPosition" : "-" + (i % 4 * 100) + "px" + " " + "-" + (parseInt(i / 4) * 100) + "px",
+				"backgroundImage"    : "url('" + img + "')"
+			});
+		}
+	}
+	else {
+		document.getElementById("formsg").innerHTML = "You haven't won yet..";
+	}
+	
+}
+//show the check win button
+function showButton() {
+	$("#buttonWin").show();
+}
